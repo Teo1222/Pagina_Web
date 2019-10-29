50cfcaba9829468479c47faab586627d6255b09c
andm13 <andrescev13@gmail.com>
2019-10-29 18:14:59 -0500

    Se arreglo errores y se aumento funcionalidad a la lista de tareas

---
+++ main.js
@@ -0,0 +1,55 @@
+// pone boton cerrar en todos los elementos de la lista
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var value = document.getElementById("ingreso").value;
+  var t = document.createTextNode(value);
+  li.appendChild(t);
+  if (value === '') {
+    alert("Debe ingresar tarea");
+  } else {
+    document.getElementById("tareas").appendChild(li);
+  }
+  document.getElementById("ingreso").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
